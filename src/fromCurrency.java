b046b865547d154002a72b932e86460d57dc233f
giorgizena123 <zenaishvili621@gmail.com>
2025-04-22 21:40:19 +0400

    Update fromCurrency.java

--- src/fromCurrency.java
+++ src/fromCurrency.java
@@ -1,14 +1,1 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class cvla extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
-}
